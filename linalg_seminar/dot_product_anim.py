0866b3d211753dfcf2e65d015e78afce5b2d5772
Adam Klepac <djklepy@gmail.com>
2024-02-20 16:53:22 +0100

    updated dot product anim

--- linalg_seminar/dot_product_anim.py
+++ linalg_seminar/dot_product_anim.py
@@ -4,7 +4,7 @@ from manim import *
 from dot_product_config import *
 
 
-class DotProduct(Scene):
+class DotProduct(MovingCameraScene):
     def get_axes(self):
         axes = Axes(
             x_range=[-6, 6],
@@ -15,12 +15,12 @@ class DotProduct(Scene):
 
     def get_vectors(self, axes):
         vectors = VGroup(
+            Vector(axes.coords_to_point(-2, 2), color=Colors.vectors),
             Vector(axes.coords_to_point(3, 2), color=Colors.vectors),
-            Vector(axes.coords_to_point(3, -2), color=Colors.vectors),
-            Vector(axes.coords_to_point(-1, 2), color=Colors.vectors),
+            Vector(axes.coords_to_point(3.5, -2), color=Colors.vectors),
             Vector(axes.coords_to_point(-1, -1), color=Colors.vectors),
             Vector(axes.coords_to_point(1, 1), color=Colors.vectors),
-            Vector(axes.coords_to_point(-3, 0), color=Colors.vectors),
+            Vector(axes.coords_to_point(-3, 1), color=Colors.vectors),
         )
 
         return vectors
@@ -35,17 +35,13 @@ class DotProduct(Scene):
         vectors = self.get_vectors(axes)
         self.play(Create(vectors))
 
+        dots = VGroup(*[Dot(v.get_end(), color=Colors.vectors) for v in vectors])
+
         self.wait()
 
-        self.play(
-            *[
-                Transform(
-                    vectors[i],
-                    Dot(vectors[i].get_end(), color=Colors.vectors),
-                )
-                for i in range(len(vectors))
-            ]
-        )
+        self.play(*[Transform(vectors[i], dots[i]) for i in range(len(vectors))])
+
+        self.wait()
 
         # Projection Line
         self.next_section(
@@ -70,12 +66,49 @@ class DotProduct(Scene):
         )
         self.play(Create(u_line))
 
-        # Perpendicular lines
+        self.wait()
+
+        # Perpendicular lines and projections
         self.next_section(
             name="Perpendicular Lines", skip_animations=SkipScene.perpendicular_lines
         )
 
-        perp_lines = VGroup()
-        self.add(perp_lines)
+        dot_projections = VGroup(
+            *[
+                Dot(u_line.get_projection(d.get_center()), color=Colors.projections)
+                for d in dots
+            ]
+        )
+        perp_lines = VGroup(
+            *[
+                DashedLine(
+                    dots[i].get_center(),
+                    dot_projections[i].get_center(),
+                    color=Colors.vectors,
+                )
+                for i in range(len(dots))
+            ]
+        )
+        self.play(*[Create(line) for line in perp_lines])
+
+        self.wait()
+
+        self.play(*[Transform(dots[i], dot_projections[i]) for i in range(len(dots))])
+
+        self.wait()
+
+        # Zoom in scene
+        self.next_section(name="Zoom In", skip_animations=SkipScene.zoom_in)
+
+        self.play(
+            *[FadeOut(perp_lines[i]) for i in range(1, len(perp_lines))],
+            *[FadeOut(dots[i]) for i in range(1, len(dots))],
+            *[FadeOut(vectors[i]) for i in range(1, len(vectors))]
+        )
+
+        self.camera.frame.save_state()
+        self.play(
+            self.camera.frame.animate.set(width=8).move_to(u.get_end()),
+        )
 
         self.wait()
--- linalg_seminar/dot_product_config.py
+++ linalg_seminar/dot_product_config.py
@@ -7,10 +7,12 @@ class Colors:
     axes = BLUE
     vectors = PINK
     projection_line = YELLOW
+    projections = TEAL
 
 
 @dataclass
 class SkipScene:
     many_dots: bool = True
     projection_line: bool = True
-    perpendicular_lines: bool = False
+    perpendicular_lines: bool = True
+    zoom_in: bool = True
